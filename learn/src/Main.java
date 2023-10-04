ac62aa6d550fdc6a846f5ef799205caa3858d2c4
MOYU111 <2867253689@qq.com>
2023-10-04 21:46:59 +0800

    add:complete work1

--- learn/src/Main.java
+++ learn/src/Main.java
@@ -1,5 +1,13 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+        int f,i;
+        int c;
+        Scanner scan=new Scanner(System.in);
+        c=scan.nextInt();
+        f=(int)(1.44*c/44.196);
+        i=(int)(12*(0.01/0.3048*c-f));
+        System.out.println((int)f+" "+(int)i);
     }
-}
+} 
+
